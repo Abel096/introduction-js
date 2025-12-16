8f0d82d4e79a96da7223a3a84c80ebbe9deca6f4
Abel Beltran <abel.bd096@gmail.com>
2025-12-16 12:42:40 -0600

    const

---
+++ js/02.js
@@ -0,0 +1,8 @@
+/* ========== Variables with const ========== */
+
+// Using const
+const client = "Abel";
+// You can't reasign a value to a const.
+// Const must be initialized.
+const price = 1000;
+console.log(price);
